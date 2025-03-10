8d08e5ce28de31518bce9bc2aa0b4c4d302a4407
juan7180 <113482741+juan7180@users.noreply.github.com>
2025-03-09 20:56:02 -0400

    add source code

---
+++ config.py
@@ -0,0 +1,13 @@
+class Config:
+    DISCORD_CLIENT_ID : str = 1234567890 #Put your client ID here
+    DiscordBotToken : str = "Put Your Bot Token Here"
+    DISCORD_CLIENT_SECRET : str = "Put your client secret here"
+
+    BackendServerUrl : str = "Put your SUBTER WEB URL here"
+    BackendAuthenticationToken : str = "Put your SUBTER WEB Authentication Token here"
+
+    AuthorisedGuilds : list = [1234567890] #Put your guild ID here
+
+    VerifiedRoleId : int = 1234567890 #Put your Verified Role
+    OBCRoleId : int = 1234567890 #Put your OBC Role ID Here
+    ServerBoosterRole : int = 1234567890 #Put your Server Booster Role ID Here
